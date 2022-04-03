aec925938bd1ae0dbafdaca1dd153451660e431e
yunCrush <yunfei1577266641@gmail.com>
2022-04-04 01:17:01 +0800

    CompletableFuture

---
+++ src/main/java/com/yun/CompletableFutureTest.java
@@ -0,0 +1,9 @@
+package com.yun;
+
+/**
+ * Author: yunCrush
+ * Date:2022/4/4 0:08
+ * Description:
+ */
+public class CompletableFutureTest {
+}
---
+++ src/main/java/com/yun/FutureTaskTest.java
@@ -0,0 +1,9 @@
+package com.yun;
+
+/**
+ * Author: yunCrush
+ * Date:2022/4/3 23:32
+ * Description:
+ */
+public class FutureTaskTest {
+}
